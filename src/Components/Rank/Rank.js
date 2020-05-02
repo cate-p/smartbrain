94e1a88ea40d7771c191aed1fca95eae63ce7e5d
Caterina <caterina.pampolari@live.it>
2020-05-02 15:51:58 +0200

    latest changes

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({ name, entries }) => {
 	return (
 		<div>
-			<div className="white f3">{`${name}, your current entry count is...`}</div>
+			<div className="white f3 pa4">{`${name}, your current entry count is...`}</div>
 			<div className="pt3 white f1">{`${entries}`}</div>
 		</div>
 	);
